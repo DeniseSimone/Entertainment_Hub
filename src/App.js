9e23ee0eb27650f5187a10d2c5dfdde51c18739b
DeniseSimone <denisesimone1997@gmail.com>
2022-03-20 09:12:17 +0000

    initialized entertainment hub app, deleted unnecessary files, and set up files for the project

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+
+// import components
+import Header from './components/Header';
+
+// import icons
+// import { BsFillCameraReelsFill } from "react-icons/bs";
+{/* <BsFillCameraReelsFill /> */}
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import '../styles/Header.css'
+
+const Header = () => {
+    return <span className='header'>ENTERTAINMENT HUB</span>
+}
+
+export default Header;
